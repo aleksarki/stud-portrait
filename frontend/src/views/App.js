0e78217094ad409332f1876afb7341a0053d8e66
aleksarki <sashas200@yandex.ru>
2025-10-07 18:22:26 +0500

    frontend: moved files

---
+++ frontend/src/views/App.js
@@ -0,0 +1,12 @@
+import logo from '../static/logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <span>First steps</span>
+    </div>
+  );
+}
+
+export default App;
